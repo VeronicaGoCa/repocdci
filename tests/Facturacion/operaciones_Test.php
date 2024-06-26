fb70b1111fddf8fba3197db8b5f932fbe6c0f68b
VeronicaGoCa <programandoenlinea@gmail.com>
2024-06-25 22:41:04 -0400

    prueba1

---
+++ tests/Facturacion/operaciones_Test.php
@@ -0,0 +1,15 @@
+<?php
+declare (strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+class operaciones_Test extends TestCase {
+    public function testFactorial():void{
+        $this->assertEquals(30,30);
+    }
+    public function testuno(): void {
+        $this->assertTrue(true);
+    }
+    public function testdos():void {
+        $this->assertTrue(true);
+    }
+}
